ad9951fad238a61be249d4ae0e0f12dac7e734bd
nikhithakennedy <nikhitha193r1a0569@gmail.com>
2025-05-06 15:46:54 +0530

    -version

---
+++ EmployeeManagementSystem/src/main/java/com/contoller/DeleteEmployee.java
@@ -0,0 +1,32 @@
+package com.contoller;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dao.EmployeeDAO;
+@WebServlet("/delete")
+public class DeleteEmployee extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id=Integer.parseInt(request.getParameter("id"));
+		EmployeeDAO dao=new EmployeeDAO();
+		try {
+			boolean status=dao.deleteByID(id);
+			if(status) {
+				dao.commit();
+				response.sendRedirect("findAll");
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
